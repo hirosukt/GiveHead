4ab8d93e5176b6678da892933ac924cda91ee131
Hirosuke <senolynn7584@gmail.com>
2021-09-14 17:50:33 +0900

    Fix bug what can't get offline player's head

--- src/main/java/hirosuke/givehead/CommandGiveHead.java
+++ src/main/java/hirosuke/givehead/CommandGiveHead.java
@@ -1,16 +1,25 @@
 package hirosuke.givehead;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.dbassett.skullcreator.SkullCreator;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+class Info {
+    public String name;
+    public String id;
+}
 
 public class CommandGiveHead implements CommandExecutor {
     @Override
@@ -22,28 +31,51 @@ public class CommandGiveHead implements CommandExecutor {
             || command.getName().equalsIgnoreCase("namakubi")
             || command.getName().equalsIgnoreCase("skull")
             || command.getName().equalsIgnoreCase("playerskull")) {
+
             Player player = null;
-            Player targetPlayer = Bukkit.getPlayer(args[0]);
+            String targetPlayerName = null;
 
             if(sender instanceof Player) {
                 player = ((Player) sender).getPlayer();
             }
 
             if(args.length == 0) {
-                targetPlayer = player;
+                targetPlayerName = player.getName();
+            } else {
+                targetPlayerName = args[0];
             }
 
-            ItemStack item = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
-            SkullMeta skull = (SkullMeta) item.getItemMeta();
+            ObjectMapper mapper = new ObjectMapper();
 
-            skull.setDisplayName(player.getName());
-            ArrayList<String> lore = new ArrayList<String>();
-            lore.add(targetPlayer.getName() + "'s Head");
-            skull.setLore(lore);
-            skull.setOwner(targetPlayer.getName());
-            item.setItemMeta(skull);
+            try {
+                URL url = new URL("https://api.mojang.com/users/profiles/minecraft/" + targetPlayerName);
+                HttpURLConnection http = (HttpURLConnection)url.openConnection();
+                http.setRequestMethod("GET");
+                http.connect();
 
-            player.getInventory().addItem(item);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(http.getInputStream()));
+                String xml = "", line = "";
+                while((line = reader.readLine()) != null)
+                    xml += line;
+                System.out.println(xml);
+
+                Info info = mapper.readValue(xml, Info.class);
+                System.out.println(info.name + ", " + info.id);
+
+                reader.close();
+
+                Player targetPlayer = Bukkit.getOfflinePlayer(info.name).getPlayer();
+                ItemStack item = SkullCreator.itemFromName(info.name);
+//                SkullMeta meta = (SkullMeta) item.getItemMeta();
+//                meta.setOwningPlayer(targetPlayer);
+//                meta.setDisplayName(targetPlayerName + "'s Head");
+//                item.setItemMeta(meta);
+
+                player.getInventory().addItem(item);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
             return true;
         }
         return false;
